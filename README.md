0fc3a04f6762b0383055b75e6dbdf61961b0f6da
Riley <94302477+Rilaey@users.noreply.github.com>
2022-12-07 17:13:45 -0500

    started working on read me file and continued working on init function

---
+++ README.md
@@ -0,0 +1,37 @@
+  # Team Profile Generator
+
+  ## Table of Contents:
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description: 
+  -  Build your dream team with Team Profile Generator! During the process of building this application I built on my knowledge of object oriented programming along with working on my problem solving skills. With this application it takes away the heavy lifting for the user. The application takes in user input of team members and generates the provided information on a prestyled html page.
+
+  ## Installation:
+  - You must have the following for the application to function correctly: 
+  - To run this application, you must have node installed.
+
+  ## Usage
+  - After cloning the code repo for use, start the application by running "node index" in the terminal.
+
+  ## License
+  - license : MIT ![License MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  - https://opensource.org/licenses/MIT
+
+  ## Contributions
+  - None.
+
+  ## Tests
+  - Tests for this application can be found in the "tests" folder in the directory. You can run these these suites by entering "npm test" in the terminal"
+
+  ## Questions
+  - Have any questions? You can contact me here.
+  - Github: https://github.com/Rilaey
+  - Email: xrileynewhart@gmail.com
--- index.js
+++ index.js
@@ -12,6 +12,63 @@ const Engineer = require('./lib/engineer');
 const generateHTML = require('./src/generateHTML');
 
 const managerQuestions = [
+    {
+        name: 'name',
+        message: 'What is your name?',
+        type: 'input',
+        validate: nameInput => {
+            if(nameInput) {
+                return true
+            } else {
+                console.log('Please input your name.')
+            }
+        }
+    },
+    {
+        name: 'id',
+        message: 'What is your employee ID?',
+        type: 'input',
+        validate: idInput => {
+            if(idInput) {
+                return true
+            } else {
+                console.log('Please input your employee ID.')
+            }
+        }
+    },
+    {
+        name: 'email',
+        message: 'What is your email?',
+        type: 'input',
+        validate: emailInput => {
+            if(emailInput) {
+                return true
+            } else {
+                console.log('Please input your email.')
+            }
+        }
+    },
+    {
+        name: 'office',
+        message: 'What is your office number',
+        type: 'input',
+        validate: officeInput => {
+            if(officeInput) {
+                return true
+            } else {
+                console.log('Please input your office number.')
+            }
+        }
+    },
+    {
+        name: 'members',
+        message: 'Are there more members of your team?',
+        type: 'list',
+        choices: ['Engineer', 'Intern', 'No, finish building my team.']
+    }
+]
+
+const engineerQuestions = [
         {
             name: 'name',
             message: 'What is your name?',
@@ -49,32 +106,126 @@ const managerQuestions = [
             }
         },
         {
-            name: 'office',
-            message: 'What is your office number',
+            name: 'github', 
+            message: 'What is your github user name?',
             type: 'input',
-            validate: officeInput => {
-                if(officeInput) {
+            validate: gitHubInput => {
+                if(gitHubInput) {
                     return true
                 } else {
-                    console.log('Please input your office number.')
+                    console.log('Please enter your github user name.')
                 }
             }
         },
         {
-            name: 'members',
-            message: 'Are there more members of your team?',
+            name: 'newMember',
+            message: 'Would you like to add new team members?',
             type: 'list',
             choices: ['Engineer', 'Intern', 'No, finish building my team.']
         }
-    ];
+    ]
 
-// function init() {
-//     inquirer.prompt(managerQuestions)
+const internQuestions = [
+    {
+        name: 'name',
+        message: 'What is your name?',
+        type: 'input',
+        validate: nameInput => {
+            if(nameInput) {
+                return true
+            } else {
+                console.log('Please input your name.')
+            }
+        }
+    },
+    {
+        name: 'id',
+        message: 'What is your employee ID?',
+        type: 'input',
+        validate: idInput => {
+            if(idInput) {
+                return true
+            } else {
+                console.log('Please input your employee ID.')
+            }
+        }
+    },
+    {
+        name: 'email',
+        message: 'What is your email?',
+        type: 'input',
+        validate: emailInput => {
+            if(emailInput) {
+                return true
+            } else {
+                console.log('Please input your email.')
+            }
+        }
+    },
+    {
+        name: 'school',
+        message: 'What school do you attend?',
+        type: 'input',
+        validate: schoolInput => {
+            if(schoolInput) {
+                return true
+            } else {
+                console.log('Please input your school.')
+            }
+        }
+    },
+    {
+        name: 'newMember',
+        message: 'Would you like to add new team members?',
+        type: 'list',
+        choices: ['Engineer', 'Intern', 'No, finish building my team.']
+    }
+]
+
+// const returnQuestions = [
+//     {
+//         name: 'newMember',
+//         message: 'Would you like to add new team members?',
+//         type: 'list',
+//         choices: ['Engineer', 'Intern', 'No, finish building my team.']
+//     }
+// ]
+
+function init() {
+    inquirer.prompt(managerQuestions)
+    .then(function(data) {
+        //console.log(data)
+    if(data.members == 'Engineer') {
+        inquirer.prompt(engineerQuestions)
+        .then(function(value) {
+            if(value.newMember == 'Engineer') {
+                return inquirer.prompt(engineerQuestions)
+            }
+        })
+    } else if(data.members == 'Intern') {
+        inquirer.prompt(internQuestions)
+        .then(function(value) {
+            if(value.newMember == 'Engineer') {
+                return inquirer.prompt(internQuestions)
+            }
+        })
+    } else if(data.members == 'No, finish building my team.') {
+        console.log('Team built! Success!')
+    }
+    
+    })
+}
+
+// function returnNow() {
+//     inquirer.prompt(returnQuestions)
 //     .then(function(data) {
-//         if(data.choices == 'Intern') {
-//             inquirer.prompt(managerQuestions)
-//         }
+//         //console.log(data)
+//     if(data.newMember == 'Engineer') {
+//         inquirer.prompt(engineerQuestions)
+//     } else if(data.newMember == 'Intern') {
+//         inquirer.prompt(internQuestions)
+//     }    
 //     })
 // }
 
-// init()
+init()
